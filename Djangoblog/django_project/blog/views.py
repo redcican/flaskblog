5c512269e2ee6922a7b52c0951884500bf920534
ChenShikun <cican17@gmail.com>
2020-05-01 21:27:31 +0200

    database and migration

--- Djangoblog/django_project/blog/views.py
+++ Djangoblog/django_project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS', 
-        'title': 'Blog post 1', 
-        'content': 'First post content', 
-        'date_posted':'Auger 27, 2018'
-    },
-    {
-        'author': 'Jane Doe', 
-        'title': 'Blog post 2', 
-        'content': 'Second post content', 
-        'date_posted':'Auger 29, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
