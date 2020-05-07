68f3b38cb50f3a54f9f9a2359da3531d50702490
ChenShikun <cican17@gmail.com>
2020-05-08 01:27:38 +0200

    add pagenation

--- Djangoblog/django_project/blog/urls.py
+++ Djangoblog/django_project/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,PostDeleteView,UserPostListView
 from . import views
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 ]
